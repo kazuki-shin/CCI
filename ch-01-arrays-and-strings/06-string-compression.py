37165bb01add8fea7fd5225ef8ac695875ec66c9
Kazuki Shin <kazukis2@illinois.edu>
2019-06-15 21:32:48 -0700

    string compression

--- ch-01-arrays-and-strings/06-string-compression.py
+++ ch-01-arrays-and-strings/06-string-compression.py
@@ -4,6 +4,23 @@
 
 # approach: put all chars in dict,
 def compress(string):
+  dict = {}
+  for char in string:
+    if char not in dict:
+      dict[char] = 1
+    else:
+      dict[char] += 1
+  encoded = []
+  for key, value in dict.items():
+    if value == 1:
+      encoded.append(key)
+    elif value == 2:
+      encoded.append(key)
+      encoded.append(key)
+    else:
+      encoded.append(key)
+      encoded.append(value)
+  return ''.join(encoded)
 
 if __name__ == "__main__":
   import sys
